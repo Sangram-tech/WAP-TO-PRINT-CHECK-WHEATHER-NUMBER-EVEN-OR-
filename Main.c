c1e546c155796b0f8cc0b1221d80c4eb9e07d82e
Sangram Mahakud <sangrammahakud1@gmail.com>
2024-10-09 19:00:05 +0530

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n ;
+    printf("Enter number:\n");
+    scanf("%d",&n);
+    if(n%2==0){
+    printf("Even number");}
+    else{
+    printf("Odd number");
+}
+    return 0;
+}
